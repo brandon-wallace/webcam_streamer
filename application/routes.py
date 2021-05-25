a61fa5ed5d93cea58f2540a4ddecd21b0292e5de
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-05-24 22:59:27 -0400

    config: Configure opencv-python
    Opencv-python was configured to work properly to stream from a
    video camera.

--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,17 @@
 # application/routes.py
 
+import cv2
 from flask import render_template, Response
 from application import app
-from application.camera import Camera
+
+
+video = cv2.VideoCapture(0)
 
 
 @app.route('/')
 def index():
+    '''Index page'''
+
     return render_template('index.html')
 
 
@@ -14,14 +19,17 @@ def generator(webcam):
     '''Get camera frames'''
 
     while True:
-        frame = webcam.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        success, image = video.read()
+        if success:
+            ret, jpeg = cv2.imencode('.jpg', image)
+            frame = jpeg.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/stream')
 def video_stream():
     '''Display video stream'''
 
-    return Response(generator(Camera()),
+    return Response(generator(video),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
